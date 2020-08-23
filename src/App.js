f0fe353a8f1eb74a03d531c00ed7d82e39291bb7
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-08-23 00:32:54 -0300

    Importando CSS e Imagem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
     return (
         <>
             <Header title='Projects' />
+
+            <img width={500} src={backgroundImage} />
 
             <ul>
                 { projects.map(project => <li key={project}>{ project }</li>) }
